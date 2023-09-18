e679e1a7157defdde202f768015bcd6bd39003bd
WillemsLars <121558591+WillemsLars@users.noreply.github.com>
2023-09-18 18:29:59 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,4 +1,21 @@
+let random_nummer = 0
 let Tellen = 0
+input.onButtonPressed(Button.B, function () {
+    random_nummer = randint(0, 6)
+    if (random_nummer == 1) {
+        basic.showString("OMTER EERST EEN GLAS WATER LEEG DRINKEN ")
+    } else if (random_nummer == 1) {
+        basic.showString("PINGPONG, EERST TOT 3")
+    } else if (random_nummer == 1) {
+        basic.showString("IN DE 20 GOOIEN ")
+    } else if (random_nummer == 1) {
+        basic.showString("OMTER EERST 6 GOOIEN MET EEN DOBELLESTEEN ")
+    } else if (random_nummer == 1) {
+        basic.showString("EEN VOORWERP VERSTOPPEN / ZOEKEN ")
+    } else if (random_nummer == 1) {
+        basic.showString("EEN SLOKJE WATER ")
+    }
+})
 basic.forever(function () {
     if (input.pinIsPressed(TouchPin.P1)) {
         basic.showIcon(IconNames.Ghost)
